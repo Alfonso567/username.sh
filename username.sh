408ea3647006dd78aca8245f8bcaf7ca4ffc3141
Alfonso Lopez <lopezpanduro@chapman.edu>
2025-10-14 18:18:24 -0700

    username.sh

--- username.sh
+++ username.sh
@@ -8,12 +8,15 @@ echo "Your username must follow these rules:"
 echo "1. Only lowercase letters, digits, and underscores are allowed."
 echo "2. It must start with a lowercase letter."
 echo "3. It must be between 3 and 12 characters long."
-username="$1"
 
-if [[ "$username" =~ ^[a-z][a-z0-9_]{2,11}$ ]]; then
-  echo "$username is a valid username."
-else
-  echo "Invalid username. Please try again."
-  exit 1
-fi
+while true; do
+  echo -n "Please enter a username: "
+  read username -r username-input
 
+  if [[ "$username" =~ ^[a-z][a-z0-9_]{2,11}$ ]]; then
+    echo "$username is a valid username."
+    break
+  else
+    echo "Invalid username. Please try again."
+  fi
+done
